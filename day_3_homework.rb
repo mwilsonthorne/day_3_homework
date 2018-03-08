502315af8bcfb8cc59ae1126acc7dfca7b673842
Matthew Thorne <mwilsonthorne@gmail.com>
2018-03-08 08:02:05 +0000

    Edited Homework File

--- day_3_homework.rb
+++ day_3_homework.rb
@@ -1,1 +1,26 @@
+# Add "Edinburgh Waverley" to the end of the array
+# Add "Glasgow Queen St" to the start of the array
+# Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+# Work out the index position of "Linlithgow"
+# Remove "Livingston" from the array using its name
+# Delete "Cumbernauld" from the array by index
+# How many stops there are in the array?
+# How many ways can we return "Falkirk High" from the array?
+# Reverse the positions of the stops in the array
+# Print out all the stops using a for loop
+
+
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+  stops.push("Edinburgh Waverly")
+  stops.unshift("Glasgow Queen St")
+  stops.insert(4.5, "Polmont")
+  p stops.find_index("Linlithgow")
+  stops.delete("Livingston")
+  p stops.count
+  p stops.reverse
+
+
+
+
+#
+  p stops
